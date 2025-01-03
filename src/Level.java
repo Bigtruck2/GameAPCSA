e1e65763c220d39b49ecfbf0bc864235a16fcabe
Bigtruck2 <bigburrito363@gmail.com>
2025-01-03 11:14:10 -0500

    initial

---
+++ src/Level.java
@@ -0,0 +1,11 @@
+public class Level {
+    private int points;
+    private
+    //returns true if player reached goal
+    public boolean goalReached()
+    { /* implementation not shown */ }
+    /** Returns the number of points (a positive integer) recorded for this level */
+    public int getPoints()
+    { /* implementation not shown */ }
+// There may be instance variables, constructors, and methods that are not shown.
+}
